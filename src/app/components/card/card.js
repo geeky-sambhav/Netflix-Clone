419e2f553bc5186062160f539e255f5522bcaeaf
Sambhav Gandhi <83087385+geeky-sambhav@users.noreply.github.com>
2023-05-10 20:25:23 +0530

    Create card.js

---
+++ src/app/components/card/card.js
@@ -0,0 +1,38 @@
+"use client"
+import React from 'react'
+import cls from 'classnames'
+import Image from 'next/image';
+import styles from "./card.module.css"
+import { motion } from "framer-motion"
+const Card = (props) => {
+    const{imgurl,size}=props
+const classMap={
+large:styles.lgItem,
+medium: styles.mdItem,
+small: styles.smItem
+
+}
+
+  return (
+    <div 
+    
+
+    className={styles.container}>
+   <motion.div
+   whileHover={{ scale: 1.2 }}
+   className={cls(styles.imgMotionWrapper,classMap[size])}>
+ <Image 
+      src={imgurl}
+      alt="Picture of the author"
+fill
+className={styles.cardImg}
+      />
+
+
+
+    </motion.div> 
+    </div>
+  )
+}
+
+export default Card
