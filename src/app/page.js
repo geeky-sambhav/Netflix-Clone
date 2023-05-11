9621c68a4315b7f89494a55a1fa4f164536216b6
Sambhav Gandhi <83087385+geeky-sambhav@users.noreply.github.com>
2023-05-11 20:12:57 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,32 @@
+"use client"
 import React from 'react'
 import Banner from './components/Banner'
 import Card from './components/card/card'
 import SectionCards from './components/card/section-cards'
-
+import styles from './page.module.css'
+import getvideos from './lib/videos'
 const page = () => {
+
+const disneyvideos=getvideos();
+
+
+
   return (
     <>
 <Banner title="Alice in Borderland" subtitle="Usagi" 
-imgurl="download.jpg"/>
+imgurl="download.webp"/>
+
+<div className={styles.sectionWrapper}>
+
+<SectionCards title="Disney" videos={disneyvideos} size="large"/>
+<SectionCards title="Disney" videos={disneyvideos} size="medium"/><SectionCards title="Disney" videos={disneyvideos} size="small"/>
+</div>
 
 
-<SectionCards title="Disney"/>
+
+
+
+
 
 </>    
 
